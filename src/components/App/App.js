0bbeb97ac7e69d7306f8bbf2dab3ce44799012a2
Sachin Yadav <sachin.y4dav@gmail.com>
2022-07-30 15:25:54 +0530

    ADD: custom Route component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import Videos from "../Videos/Videos";
 import SearchBar from "./../SearchBar/SearchBar";
 import useSearchVideos from "../../hooks/useSearchVideos";
+import Route from "./../Route/Route";
 
 const App = () => {
   const [videos, search] = useSearchVideos("cats");
 
   return (
     <>
-      <SearchBar onFormSubmit={search} />
-      <Videos videos={videos} />
+      <Route path="/">
+        <p>HOME SCREEN</p>
+      </Route>
+      <Route path="/videos">
+        <SearchBar onFormSubmit={search} />
+        <Videos videos={videos} />
+      </Route>
     </>
   );
 };
